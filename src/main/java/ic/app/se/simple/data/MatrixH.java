a05e829cd1d734c2d871823fcb535654a39bb324
hjh <applyhhj@163.com>
2015-10-27 17:31:00 +0800

    correct program for estimator

--- src/main/java/ic/app/se/simple/data/MatrixH.java
+++ src/main/java/ic/app/se/simple/data/MatrixH.java
@@ -15,25 +15,25 @@ import static ic.app.se.simple.common.Utils.getPQType;
  */
 public class MatrixH {
 
-//    stores index
-    public static Logger logger= LoggerFactory.getLogger(MatrixH.class);
+    //    stores index
+    public static Logger logger = LoggerFactory.getLogger(MatrixH.class);
 
-//    H column
+    //    H column
     private List<Integer> HI;
 
-//    H value
+    //    H value
     private List<Double> H;
 
-//    H row
+    //    H row
     private List<Integer> MH;
 
-//    HT row
+    //    HT row
     private List<Integer> IHT;
 
-//    column
+    //    column
     private List<Integer> MIHT;
 
-//    value
+    //    value
     private List<Double> MHT;
 
     private BranchTable branchTable;
@@ -56,7 +56,7 @@ public class MatrixH {
 
         this.branchTable = branchTable;
 
-        this.busNumbers=busNumbers;
+        this.busNumbers = busNumbers;
 
         this.measurementTable = measurementTable;
 
@@ -68,19 +68,19 @@ public class MatrixH {
 
         MH = new ArrayList<Integer>();
 
-        IHT=new ArrayList<Integer>();
+        IHT = new ArrayList<Integer>();
 
-        MIHT=new ArrayList<Integer>();
+        MIHT = new ArrayList<Integer>();
 
-        MHT=new ArrayList<Double>();
+        MHT = new ArrayList<Double>();
 
-        HTRI=new SparseMatrix();
+        HTRI = new SparseMatrix();
 
-        r=measurementTable.getNOM();
+        r = measurementTable.getNOM();
 
-        c=busNumbers.getNOB();
+        c = busNumbers.getNOB();
 
-        KPQ=matrixY.getKPQ();
+        KPQ = matrixY.getKPQ();
 
         computeHMatrix();
 
@@ -92,42 +92,42 @@ public class MatrixH {
 
         printHTMatrix();
 
-        HTRI.print("HTRI"+getPQType(KPQ));
+        HTRI.print("HTRI" + getPQType(KPQ));
 
     }
 
-//    stores index, use index to access
+    //    stores index, use index to access
     public void computeHMatrix() {
 
         MH.add(0);
 
-         for (int i = 0; i < measurementTable.getNOM(); i++) {
+        for (int i = 0; i < measurementTable.getNOM(); i++) {
 
-            int type=measurementTable.getType()[i];
+            int type = measurementTable.getType()[i];
 
-            if (matrixY.getKPQ()==1){
+            if (matrixY.getKPQ() == 1) {
 
-                switch (type){
+                switch (type) {
 
                     case 0:
 
-                    case 1:{
+                    case 1: {
 
-                        int I= getBusInternalNumber(i);
+                        int I = getBusInternalNumber(i);
 
-                        HI.add(I-1);
+                        HI.add(I - 1);
 
-                        H.add(-1/measurementTable.getV0());
+                        H.add(-1 / measurementTable.getV0());
 
                         break;
 
                     }
 
-                    case 3:{
+                    case 3: {
 
-                        int I= getBusInternalNumber(i);
+                        int I = getBusInternalNumber(i);
 
-                        List<B> bb=getBFromYMatrixForRowI(I);
+                        List<B> bb = getBFromYMatrixForRowI(I);
 
                         for (int j = 0; j < bb.size(); j++) {
 
@@ -146,7 +146,7 @@ public class MatrixH {
 
                     case 4:
 
-                    case 6:{
+                    case 6: {
 
                         break;
 
@@ -154,69 +154,69 @@ public class MatrixH {
 
                     case 5:
 
-                    case 7:{
+                    case 7: {
 
 //                        branch number, natural order
-                        int L=measurementTable.getLocation()[i];
+                        int L = measurementTable.getLocation()[i];
 
 //                        this is the number, not index
-                        int I=busNumbers.getTOI().get(branchTable.getI()[L-1]);
+                        int I = busNumbers.getTOI().get(branchTable.getI()[L - 1]);
 
-                        int J=busNumbers.getTOI().get(branchTable.getJ()[L-1]);
+                        int J = busNumbers.getTOI().get(branchTable.getJ()[L - 1]);
 
-                        double R=branchTable.getRij()[L-1];
+                        double R = branchTable.getRij()[L - 1];
 
-                        double X=branchTable.getXij()[L-1];
+                        double X = branchTable.getXij()[L - 1];
 
-                        double YK=branchTable.getYk()[L-1];
+                        double YK = branchTable.getYk()[L - 1];
 
-                        double B=X/(R*R+X*X);
+                        double B = X / (R * R + X * X);
 
-                        if (YK>0){
+                        if (YK > 0) {
 
-                            if (type==5){
+                            if (type == 5) {
 
-                                H.add(-(2*B/YK-B)/YK);
+                                H.add(-(2 * B / YK - B) / YK);
 
-                                H.add(B/YK);
+                                H.add(B / YK);
 
-                            }else {
+                            } else {
 
-                                H.add(B/YK);
+                                H.add(B / YK);
 
-                                H.add(B/YK-2*B);
+                                H.add(B / YK - 2 * B);
 
                             }
 
-                        }else {
+                        } else {
 
-                            if (type==5){
+                            if (type == 5) {
 
-                                H.add(B-2*(B-YK));
-
-                                H.add(B);
-
-                            }else {
+                                H.add(B - 2 * (B - YK));
 
                                 H.add(B);
 
-                                H.add(-B-2*YK);
+                            } else {
+
+                                H.add(B);
+
+                                H.add(-B - 2 * YK);
 
                             }
 
                         }
 
-                        HI.add(I-1);
+                        HI.add(I - 1);
 
-                        HI.add(J-1);
+                        HI.add(J - 1);
 
                         break;
 
                     }
 
-                    default:{
+                    default: {
 
-                        logger.error("Unrecognized type {}!!",type);
+                        logger.error("Unrecognized type {}!!", type);
 
                         break;
 
@@ -224,9 +224,9 @@ public class MatrixH {
 
                 }
 
-            }else {
+            } else {
 
-                switch (type){
+                switch (type) {
 
                     case 0:
 
@@ -236,17 +236,17 @@ public class MatrixH {
 
                     case 5:
 
-                    case 7:{
+                    case 7: {
 
                         break;
 
                     }
 
-                    case 2:{
+                    case 2: {
 
-                        int I= getBusInternalNumber(i);
+                        int I = getBusInternalNumber(i);
 
-                        List<B> bb=getBFromYMatrixForRowI(I);
+                        List<B> bb = getBFromYMatrixForRowI(I);
 
                         for (int j = 0; j < bb.size(); j++) {
 
@@ -263,25 +263,25 @@ public class MatrixH {
 
                     case 4:
 
-                    case 6:{
+                    case 6: {
 
-                        int L=measurementTable.getLocation()[i];
+                        int L = measurementTable.getLocation()[i];
 
-                        int I=busNumbers.getTOI().get(branchTable.getI()[L-1]);
+                        int I = busNumbers.getTOI().get(branchTable.getI()[L - 1]);
 
-                        int J=busNumbers.getTOI().get(branchTable.getJ()[L-1]);
+                        int J = busNumbers.getTOI().get(branchTable.getJ()[L - 1]);
 
-                        double X=branchTable.getXij()[L-1];
+                        double X = branchTable.getXij()[L - 1];
 
-                        double B=1/X;
+                        double B = 1 / X;
 
-                        if (type==4){
+                        if (type == 4) {
 
                             H.add(-B);
 
                             H.add(B);
 
-                        }else {
+                        } else {
 
                             H.add(B);
 
@@ -289,9 +289,9 @@ public class MatrixH {
 
                         }
 
-                        HI.add(I-1);
+                        HI.add(I - 1);
 
-                        HI.add(J-1);
+                        HI.add(J - 1);
 
                     }
 
@@ -305,27 +305,27 @@ public class MatrixH {
 
     }
 
-    public void computeHTMatrix(){
+    public void computeHTMatrix() {
 
         IHT.clear();
 
-        int former=0;
+        int former = 0;
 
-        for (int i = 0; i <busNumbers.getNOB(); i++) {
+        for (int i = 0; i < busNumbers.getNOB(); i++) {
 
             IHT.add(0);
 
-            if (i!=0) {
+            if (i != 0) {
 
-                IHT.set(i,IHT.get(i-1)+former);
+                IHT.set(i, IHT.get(i - 1) + former);
 
             }
 
-            former=0;
+            former = 0;
 
             for (int j = 0; j < HI.size(); j++) {
 
-                if (HI.get(j)==i) {
+                if (HI.get(j) == i) {
 
                     former++;
 
@@ -335,9 +335,9 @@ public class MatrixH {
 
             for (int m = 0; m < measurementTable.getNOM(); m++) {
 
-                Double data=getHmi(m+1,i+1);
+                Double data = getHmi(m + 1, i + 1);
 
-                if (data!=null){
+                if (data != null) {
 
 //                    this is the measurement index
                     MIHT.add(m);
@@ -350,11 +350,11 @@ public class MatrixH {
 
         }
 
-        IHT.add(busNumbers.getNOB(),IHT.get(busNumbers.getNOB()-1)+former);
+        IHT.add(busNumbers.getNOB(), IHT.get(busNumbers.getNOB() - 1) + former);
 
     }
 
-    private void computeHTRI(){
+    private void computeHTRI() {
 
         HTRI.getColumnAndValues().clear();
 
@@ -367,11 +367,11 @@ public class MatrixH {
         for (int i = 0; i < MHT.size(); i++) {
 
 //            sigma should be set before this computation
-            riv=measurementTable.getSigma()[MIHT.get(i)];
+            riv = measurementTable.getSigma()[MIHT.get(i)];
 
-            riv=1/riv/riv;
+            riv = 1 / riv / riv;
 
-            HTRI.getColumnAndValues().add(new ColumnAndValue(MIHT.get(i),MHT.get(i)*riv));
+            HTRI.getColumnAndValues().add(new ColumnAndValue(MIHT.get(i), MHT.get(i) * riv));
 
         }
 
@@ -385,27 +385,27 @@ public class MatrixH {
         return HTRI;
     }
 
-    private int getBusInternalNumber(int i){
+    private int getBusInternalNumber(int i) {
 
         return busNumbers.getTOI().get(measurementTable.getLocation()[i]);
 
     }
 
-    private List<B> getBFromYMatrixForRowI(int busNumber){
+    private List<B> getBFromYMatrixForRowI(int busNumber) {
 
-        List<B> ret=new ArrayList<B>();
+        List<B> ret = new ArrayList<B>();
 
-        for (int j = 1; j <=busNumbers.getNOB(); j++) {
+        for (int j = 1; j <= busNumbers.getNOB(); j++) {
 
 //            get method use bus number so it starts from 1
-            Double Bvalue= matrixY.getBIJ(busNumber,j);
+            Double Bvalue = matrixY.getBIJ(busNumber, j);
 
-            if (Bvalue!=null) {
+            if (Bvalue != null) {
 
                 B b = new B();
 
 //                stores index
-                b.setJ(j-1);
+                b.setJ(j - 1);
 
                 b.setB(Bvalue);
 
@@ -419,13 +419,13 @@ public class MatrixH {
 
     }
 
-//  use number not array index as input
-    public Double getHmi(int m,int i){
+    //  use number not array index as input
+    public Double getHmi(int m, int i) {
 
-        for (int j = MH.get(m-1); j <MH.get(m); j++) {
+        for (int j = MH.get(m - 1); j < MH.get(m); j++) {
 
 //            this array stores index
-            if (HI.get(j)==i-1){
+            if (HI.get(j) == i - 1) {
 
                 return H.get(j);
 
@@ -438,12 +438,12 @@ public class MatrixH {
     }
 
     //  use number not array index as input
-    public Double getHTim(int i,int m){
+    public Double getHTim(int i, int m) {
 
-        for (int j = IHT.get(i-1); j <IHT.get(i); j++) {
+        for (int j = IHT.get(i - 1); j < IHT.get(i); j++) {
 
 //            this array stores index
-            if (MIHT.get(j)==m-1){
+            if (MIHT.get(j) == m - 1) {
 
                 return MHT.get(j);
 
@@ -455,7 +455,7 @@ public class MatrixH {
 
     }
 
-    private class B{
+    private class B {
 
         private int J;
 
@@ -479,21 +479,21 @@ public class MatrixH {
 
     }
 
-    public void printHMatrix(){
+    public void printHMatrix() {
 
-        System.out.print("\n************H"+getPQType(KPQ)+"************\n\n");
+        System.out.print("\n************H" + getPQType(KPQ) + "************\n\n");
 
         for (int m = 0; m < measurementTable.getNOM(); m++) {
 
             for (int i = 0; i < busNumbers.getNOB(); i++) {
 
-                Double data=getHmi(m+1,i+1);
+                Double data = getHmi(m + 1, i + 1);
 
-                if (data!=null){
+                if (data != null) {
 
-                    System.out.printf("%9.4f,",data);
+                    System.out.printf("%9.4f,", data);
 
-                }else {
+                } else {
 
                     System.out.print("         ,");
 
@@ -509,21 +509,21 @@ public class MatrixH {
 
     }
 
-    public void printHTMatrix(){
+    public void printHTMatrix() {
 
-        System.out.print("\n************HT"+getPQType(KPQ)+"************\n\n");
+        System.out.print("\n************HT" + getPQType(KPQ) + "************\n\n");
 
         for (int i = 0; i < busNumbers.getNOB(); i++) {
 
             for (int m = 0; m < measurementTable.getNOM(); m++) {
 
-                Double data=getHTim(i + 1, m + 1);
+                Double data = getHTim(i + 1, m + 1);
 
-                if (data!=null){
+                if (data != null) {
 
                     System.out.printf("%9.4f,", data);
 
-                }else {
+                } else {
 
                     System.out.print("         ,");
 
@@ -564,19 +564,19 @@ public class MatrixH {
     }
 
     public int getHR() {
-        return MH.size()-1;
+        return MH.size() - 1;
     }
 
     public int getHC() {
-        return IHT.size()-1;
+        return IHT.size() - 1;
     }
 
-    public int getHTR(){
-        return IHT.size()-1;
+    public int getHTR() {
+        return IHT.size() - 1;
     }
 
-    public int getHTC(){
-        return MH.size()-1;
+    public int getHTC() {
+        return MH.size() - 1;
     }
 
     public int getKPQ() {
--- src/main/java/ic/app/se/simple/estimate/main.java
+++ src/main/java/ic/app/se/simple/estimate/main.java
@@ -7,17 +7,17 @@ public class main {
 
     public static void main(String[] args) {
 
-        PowerGrid powerGrid=new PowerGrid();
+        PowerGrid powerGrid = new PowerGrid();
 
         powerGrid.initData("/home/hjh/doc/powersystem/4bus/ieee4cdftest.txt");
 
-        int i=0;
+        int i = 0;
 
-        while (i++<10){
+        while (i++ < 10) {
 
             powerGrid.measure();
 
-            System.out.print("\nEstimation "+i+"\n");
+            System.out.print("\nEstimation " + i + "\n");
 
             powerGrid.estimate();
 
